399dc0e0badc96ae87791145477ea848cd67335e
theVincenator <34357145+theVincenator@users.noreply.github.com>
2025-01-15 23:19:44 +0100

    fix 404 on empty slot img

--- app/herocard.js
+++ app/herocard.js
@@ -1,12 +1,12 @@
 import { getHeroImageLocation } from "./assetlocator";
 
 export default function HeroCard({ heroname }) {
-    const heroImage = "url(" + getHeroImageLocation(heroname) + ")";
+    const heroImage = heroname == "" ? "" : "url(" + getHeroImageLocation(heroname) + ")";
     return (
         <div style={{ backgroundImage: heroImage }} className="heroCard" >
             <div className="heroCardName">
                 {heroname}
             </div>
-        </div>
+        </div >
     )
 }
